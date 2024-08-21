21cbfb401a050127e4204faa6bca5cc838023981
Alexander Haslam <aihaslam@ucdavis.edu>
2024-08-20 17:10:11 -0700

    Fix disable websocket route

--- deployment/src/main/java/io/quarkiverse/jsonrpc/deployment/JsonRPCProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jsonrpc/deployment/JsonRPCProcessor.java
@@ -220,16 +220,17 @@ public class JsonRPCProcessor {
             HttpRootPathBuildItem httpRootPathBuildItem,
             BeanContainerBuildItem beanContainerBuildItem,
             JsonRPCMethodsBuildItem jsonRPCMethodsBuildItem) {
-        recorder.createJsonRpcRouter(beanContainerBuildItem.getValue(), jsonRPCMethodsBuildItem.getMethodsMap());
+        if (jsonRPCConfig.webSocket.enabled) {
+            recorder.createJsonRpcRouter(beanContainerBuildItem.getValue(), jsonRPCMethodsBuildItem.getMethodsMap());
 
-        // Websocket for JsonRPC comms
-        routeProducer.produce(
-                httpRootPathBuildItem.routeBuilder()
-                        .management("quarkus.json-rpc.web-socket.enabled")
-                        .route(jsonRPCConfig.webSocket.path)
-                        .routeConfigKey("quarkus.json-rpc.web-socket.path")
-                        .handler(recorder.webSocketHandler())
-                        .build());
+            // Websocket for JsonRPC comms
+            routeProducer.produce(
+                    httpRootPathBuildItem.routeBuilder()
+                            .route(jsonRPCConfig.webSocket.path)
+                            .routeConfigKey("quarkus.json-rpc.web-socket.path")
+                            .handler(recorder.webSocketHandler())
+                            .build());
+        }
     }
 
     private String getEffectiveReturnType(Type type) {
